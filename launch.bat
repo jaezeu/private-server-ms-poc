9828e9f0f81e8526ffdae13b567b0ca654d2cafa
jaezeu <jazeel_18@hotmail.com>
2023-01-01 03:10:17 +0800

    update day1

--- launch.bat
+++ launch.bat
@@ -1,6 +1,6 @@
 @echo off
 @title HeavenMS
 set PATH=C:\Program Files\Java\jdk1.8.0_202\bin;%PATH%
-set CLASSPATH=.;dist\*
-java -Xmx2048m -Dwzpath=wz\ net.server.Server
+set CLASSPATH=.;cores\*
+java -Xmx2048m -Dwzpath=wz\ -cp $CLASSPATH net.server.Server
 pause
